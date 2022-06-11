c442be8d6ccac39a89d31c791fea2c56304cd2ca
arash-aloha <arash.a@live.se>
2022-06-11 22:33:24 +0200

    navbar adjustments

--- js/script.js
+++ js/script.js
@@ -10,11 +10,13 @@ navToggle.addEventListener("click", () => {
   navToggle.classList.toggle("active");
 });
 
-// fade in hero img
+const activePage = window.location.pathname;
+console.log("active page:", activePage)
+// fade in hero
 const heroImg = document.getElementById("heroImg");
 const heroText = document.getElementById("heroText");
-window.setInterval(displayImg, 1200);
-window.setInterval(displayScrollText, 1900);
+window.setTimeout(displayImg, 1200);
+window.setTimeout(displayScrollText, 1900);
 function displayImg() {
   heroImg.style.opacity = 1;
 }
@@ -22,7 +24,19 @@ function displayScrollText() {
   heroText.style.opacity = 1;
 }
 
+// active hyperlink
+const links = document.querySelectorAll(".primary-navigation li a");
 
+console.log(links)
+links.forEach(link => {
+  console.log("in the loop", link)
+    if(link.href.includes(`${activePage}`)) {
+      linkItem.classList.add('active');
+    }
+    console.log("after the loop", link)
+  });
+  console.log("active page", activePage)
+  
 
 // mouse scroll nav
 let prevScrollpos = window.pageYOffset;
